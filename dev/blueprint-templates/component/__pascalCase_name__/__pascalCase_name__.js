f8acbfc06c50cfb938d00324366dafe611f19af6
Wojciech Bielecki <wojbiel833@gmail.com>
2021-09-18 11:57:28 +0200

    Change trmplate

--- dev/blueprint-templates/component/__pascalCase_name__/__pascalCase_name__.js
+++ dev/blueprint-templates/component/__pascalCase_name__/__pascalCase_name__.js
@@ -32,6 +32,6 @@ Component.propTypes = {
 
 export {
   Component as {{pascalCase name}},
-  // Container as {{pascalCase name}},
+  // Container as {{pascalCase name}}Container,
   Component as {{pascalCase name}}Component,
 };
